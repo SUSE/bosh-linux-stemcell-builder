8aa9f4ed12c14dcbd62c4839d5d010a7b2f81e65
Dimitris Karakasilis <jimmykarily@gmail.com>
2017-08-31 13:27:12 +0000

    Update the checksum to match the rebased bosh-agent

--- stemcell_builder/stages/bosh_go_agent/apply.sh
+++ stemcell_builder/stages/bosh_go_agent/apply.sh
@@ -30,7 +30,7 @@ if is_ppc64le; then
   echo "353a455d7a6aa487ab10346cd13d0b8904e080e45ce745a514e59a20ea0b70f0  bosh-agent" | shasum -a 256 -c -
 else
   curl -L -o bosh-agent "https://s3.eu-central-1.amazonaws.com/opensuse-stemcells/bosh-agent/bosh-agent"
-  echo "972c90ecce7b14b00c6080fc1bd6f933f139369c58ae17ff3bb075ba6891e442  bosh-agent" | shasum -a 256 -c -
+  echo "4a3f11544e2ebb9526cb7f62cfdb10ef709df528888cd24d45cf65f827a9a389  bosh-agent" | shasum -a 256 -c -
 fi
 mv bosh-agent $chroot/var/vcap/bosh/bin/
 
