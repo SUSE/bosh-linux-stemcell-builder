f8276299f58ec84ba954d67b0fe27fcc99859e17
Tim Hardeck <thardeck@suse.de>
2017-09-21 16:31:15 +0200

    Fix wrong bosh-agent signature

--- stemcell_builder/stages/bosh_go_agent/apply.sh
+++ stemcell_builder/stages/bosh_go_agent/apply.sh
@@ -30,7 +30,7 @@ if is_ppc64le; then
   echo "e1177c15c00b89ef761c00705a7b041a25515b863052765b455e60f0ca504940  bosh-agent" | shasum -a 256 -c -
 else
   curl -L -o bosh-agent "https://s3.eu-central-1.amazonaws.com/opensuse-stemcells/bosh-agent/bosh-agent"
-  echo "972c90ecce7b14b00c6080fc1bd6f933f139369c58ae17ff3bb075ba6891e442  bosh-agent" | shasum -a 256 -c -
+  echo "4a3f11544e2ebb9526cb7f62cfdb10ef709df528888cd24d45cf65f827a9a389  bosh-agent" | shasum -a 256 -c -
 fi
 mv bosh-agent $chroot/var/vcap/bosh/bin/
 
